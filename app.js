7b61f0d663d79cda8b329d21bcd548dabc411431
calvindiaz <ga2023135619@gmail.com>
2023-05-25 00:54:44 +0800

    add modal iframe window

--- app.js
+++ app.js
@@ -26,9 +26,55 @@ const swiper = new Swiper(".swiper", {
 
 let secondSwiper = new Swiper(".mySwiper", {
   slidesPerView: 5,
-  // centeredSlides: false,
+
   freeMode: true,
   spaceBetween: 150,
   grabCursor: true,
-  loop: true,
+  // loop: true,
 });
+
+let video = document.querySelector(".film");
+let videoSecond = document.querySelector(".film2");
+let videoThird = document.querySelector(".film3");
+let videoFour = document.querySelector(".film4");
+let videoFive = document.querySelector(".film5");
+
+function modal() {
+  video.style.display = "block";
+}
+
+function modalSecond() {
+  videoSecond.style.display = "block";
+}
+
+function modalThird() {
+  videoThird.style.display = "block";
+}
+
+function modalFour() {
+  videoFour.style.display = "block";
+}
+
+function modalFive() {
+  videoFive.style.display = "block";
+}
+
+function closeBtn() {
+  video.style.display = "none";
+}
+
+function closeBtn2() {
+  videoSecond.style.display = "none";
+}
+
+function closeBtn3() {
+  videoThird.style.display = "none";
+}
+
+function closeBtn4() {
+  videoFour.style.display = "none";
+}
+
+function closeBtn5() {
+  videoFive.style.display = "none";
+}
